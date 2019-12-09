ea350554b35306582c8e155fa860843bb3e37ace
Yining Wang <yining.wang@utexas.edu>
2019-12-09 16:33:27 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.querySelector("main#main");
+main.remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "is the champion";
